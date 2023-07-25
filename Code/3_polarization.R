2578798b2413f933b8032e90750fb4f45f57108a
rafceegee <rafceegee@gmail.com>
2023-07-25 17:43:46 -0400

    created primary news source variable and also ran analyses with that variable

--- Code/3_polarization.R
+++ Code/3_polarization.R
@@ -2,6 +2,27 @@
 source("Code/1_load_on18.R")
 #Always a good idea to throw a check in here.
 names(on18)
+
+#### Primary News Source ####
+
+on18 %>%
+  mutate(Primary_media=case_when(
+    (primarynews_7 == 1 | primarynews_6 == 1) & (primarynews_5 == 0 & primarynews_4 == 0 & primarynews_3 == 0 & primarynews_2 == 0 & primarynews_1 == 0) ~ "Social_Media",
+    (primarynews_4 == 1 | primarynews_5 == 1) & (primarynews_6 == 0 & primarynews_7 == 0 & primarynews_3 == 0 & primarynews_2 == 0 & primarynews_1 == 0) ~ "Online",
+    (primarynews_3 == 1 | primarynews_2 == 1 | primarynews_1 == 1) & (primarynews_5 == 0 & primarynews_4 == 0 & primarynews_6 == 0 & primarynews_7 == 0) ~ "Legacy",
+    TRUE ~ "Mixed"
+    
+  ))->on18
+
+on18 %>%
+  mutate(Primary_media2=case_when(
+    (primarynews_7 == 1 | primarynews_6 == 1 | primarynews_4 == 1 | primarynews_5 == 1) & (primarynews_3 == 0 & primarynews_2 == 0 & primarynews_1 == 0) ~ "Online",
+    (primarynews_3 == 1 | primarynews_2 == 1 | primarynews_1 == 1) & (primarynews_5 == 0 & primarynews_4 == 0 & primarynews_6 == 0 & primarynews_7 == 0) ~ "Legacy",
+    TRUE ~ "Mixed"
+    
+  ))->on18
+
+table(on18$Primary_media)
 #### Political Knowledge #### 
 #Create one political knowledge variable using the variables on political knowledge.
 # R gets 1 if they got each response right
@@ -73,6 +94,11 @@ on18 %>%
 
 table(on18$Social_Use2, on18$pol_knowledge)
 
+on18 %>%
+  group_by(Primary_media) %>%
+  summarise(mean = mean(pol_knowledge, na.rm = T), sd = sd(pol_knowledge, na.rm = T)) 
+
+
 #### Policy polarization ####
 # R social media users frothing at the mouth extremists
 # Pick good policy measures from data 
@@ -99,19 +125,19 @@ range01 <- function(x, ...){(x-min(x, ...))/(max(x, ...)-min(x, ...))}
 policies <- range01(policies, na.rm = T)
 
 policies %>%  
-  bind_cols(., on18$Social_Use2)->policies
+  bind_cols(., on18$Social_Use2)->policies_social_use
 
-names(policies)<-c(policy_names, "Social_Use2")
+names(policies_social_use)<-c(policy_names, "Social_Use2")
 
-policies %>%
+policies_social_use %>%
   group_by(Social_Use2) %>%
-  summarise(across(everything(), sd, na.rm = T), .groups = "drop") -> policies_sd
+  summarise(across(everything(), sd, na.rm = T), .groups = "drop") -> policies_sd_social_use
 
 
 #Calculate standard deviation for each. 
 # Take on18
 # pivot down the agreement variables after rename
-policies_sd_down <- policies_sd %>%
+policies_sd_social_use_down <- policies_sd_social_use %>%
   pivot_longer(cols = !Social_Use2,
                names_to = "Policy_Issue", 
                values_to = "Standard_Deviation")
@@ -134,23 +160,65 @@ policies_sd_down <- policies_sd %>%
 
 #View(policies_sd_down)
 
-policies_sd_down$Policy_Issue <- factor(policies_sd_down$Policy_Issue, levels = c(policy_names, "climate_views"), 
+policies_sd_social_use_down$Policy_Issue <- factor(policies_sd_social_use_down$Policy_Issue, levels = c(policy_names, "climate_views"), 
                                         labels = c("The Government Should Help Racial Minorities", "The Government Should Help Women", 
                                                    "Higher Coporate Taxes are Good", "Higher Personal Taxes are Good",  "Health Care Should be Privatized", 
                                                    "Increasing Minimum Wage Increases Prices", "The Government Should Reduce Income Inequality", "More Business Benefits Everyone", 
                                                    "Youth Should Receive Drug Benefits", "Post Secondary Education Should be Free", "Sex-Ed Curriculum is Inappropriate", "Climate Change"))
 
+policies %>%  
+  bind_cols(., on18$Primary_media)->policies_media
+
+names(policies_media)<-c(policy_names, "Primary_media")
+
+policies_media %>%
+  group_by(Primary_media) %>%
+  summarise(across(everything(), sd, na.rm = T), .groups = "drop") -> policies_sd_media
+
+
+#Calculate standard deviation for each. 
+# Take on18
+# pivot down the agreement variables after rename
+policies_media_down <- policies_sd_media %>%
+  pivot_longer(cols = !Primary_media,
+               names_to = "Policy_Issue", 
+               values_to = "Standard_Deviation")
+#This is what we have now
+# social_use | agreement_1 | agreement_2 |
+# Heavy | 4 | 3|
+#What we want is 
+
+# social_use | Variable | Value |
+# Heavy | agreement_1 | 4|
+# Heavy | agreement_2 | 3 |
+
+# Wide (current) versus (tidy or long ) data
+
+# https://towardsdatascience.com/coding-in-r-pivot-painlessly-32e40a0b6c3d
+
+# Then Group by Social_Use and variable
+# Summarize by sd()
+
+
+#View(policies_sd_down)
+
+policies_media_down$Policy_Issue <- factor(policies_media_down$Policy_Issue, levels = c(policy_names, "climate_views"), 
+                                                   labels = c("The Government Should Help Racial Minorities", "The Government Should Help Women", 
+                                                              "Higher Coporate Taxes are Good", "Higher Personal Taxes are Good",  "Health Care Should be Privatized", 
+                                                              "Increasing Minimum Wage Increases Prices", "The Government Should Reduce Income Inequality", "More Business Benefits Everyone", 
+                                                              "Youth Should Receive Drug Benefits", "Post Secondary Education Should be Free", "Sex-Ed Curriculum is Inappropriate", "Climate Change"))
+
 
 #### GRAPH STANDARD DEVIATIONS BY POLICY ISSUE ####
 
 # I am trying to understand why you are excluding these rows
-policies_sd_down [-c(97:108), ]
-policies_sd_down[97:108,]
+#policies_sd_down [-c(97:108), ]
+#policies_sd_down[97:108,]
 #OK, these seme to come from respondents who did not respond to their social media use
 #So we can exclude them. 
 #Here is a quicker way to exclude them
-policies_sd_down
-policies_sd_down %>% 
+policies_sd_social_use_down
+policies_sd_social_use_down %>% 
   filter(complete.cases(.)) %>% 
   #instead of setting x to be zero I want x to be the Standard Deviation
   ggplot(., aes(x = Standard_Deviation, 
@@ -187,16 +255,20 @@ policies_sd_down %>%
 # scale_linetype_manual(values = c("twodash","twodash","longdash", "longdash", "dashed", "dashed", "solid", "solid")) -> policy_issue_sd_graph
 ggsave(plot = policy_issue_sd_graph, "Plots/policy_issues_sd.png", width = 12, height = 7)
 
-#alternative graph
-#policies_sd_down[-c(25:48, 61:72, 97:108), ] %>%
-#  ggplot(., aes(x = Policy_Issue, y = 0, colour = Social_Use, linetype = Social_Use)) + 
-#  geom_errorbar(aes (ymin = 0 - Standard_Deviation, ymax = 0 + Standard_Deviation), width =.2, 
-#                position = position_dodge(.8)) + labs(x = "Policy Issue", y = "Standard Deviation", colour = "Social Media Use", linetype = "Social Media Use") +
-#  scale_linetype_manual(values = c("solid", "dashed", "solid", "dotted", "dashed")) + 
-#  scale_colour_manual(values = c("black", "black", "grey", "grey", "grey")) + 
-#  geom_vline(xintercept = c(1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5, 11.5))+ coord_flip() + theme_bw()  + theme(axis.text.y=element_text(angle = 20, vjust = 0.5), legend.position ="bottom") + 
-#  guides(colour = guide_legend(nrow = 3, byrow = T, title.position = "top", hjust = 0.5, reverse = T), linetype = guide_legend(nrow = 3, byrow = T, title.position = "top", hjust = 0.5, reverse = T)) 
 
+policies_media_down %>% 
+  filter(complete.cases(.)) %>% 
+  ggplot(., aes(x = Standard_Deviation, 
+                y = Primary_media, 
+                colour = Primary_media)) + 
+  geom_point()+
+  facet_wrap(~Policy_Issue, labeller = label_wrap_gen(width=30)) +
+  labs(x = "Standard Deviation", y= "Primary Media Source") + 
+  theme_bw()  + 
+  theme(axis.text.y=element_blank(),
+        axis.ticks.y = element_blank(), 
+        strip.text = element_text(size=8))+
+  guides(color=guide_legend(reverse=T))
 
 #### Affective Polarization ####
 # We need the party feeling scores
@@ -284,3 +356,7 @@ mean_affect_scores %>%
         strip.text = element_text(size=8))+
   guides(color=guide_legend(reverse=T)) -> affect_scores_social_use
 
+
+on18 %>%
+  group_by(Primary_media2) %>%
+  summarise(mean = mean(Soc_dis, na.rm = T), sd = sd(Soc_dis, na.rm = T))
